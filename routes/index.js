62f76e4ed103b6a13daf165bef76ee0add85e8f3
Danijel <Danijel>
2016-11-30 20:57:21 +0100

    - test phantomjs

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'test12' });
+  res.render('index', { title: 'dsdsssdsd' });
 });
 router.get('/test', function(req, res, next) {
   res.render('index', { title: 'testd' });
